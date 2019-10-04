fbf2aea91de45049f1215e833f38fc80844f0b42
勝木航 <a17dc050@dhw.ac.jp>
2019-10-04 22:19:22 +0900

    feat: add react-swipe-card types

---
+++ src/type/react-swipe-card.d.ts
@@ -0,0 +1,19 @@
+declare module 'react-swipe-card' {
+  export const Cards: React.ComponentClass<CardsProps>;
+  export const Card: React.ComponentClass<CardProps>;
+  interface CardsProps {
+    className?: string;
+    onEnd?: () => void;
+    alertRight?: JSX.Element;
+    alertLeft?: JSX.Element;
+    alertTop?: JSX.Element;
+    alertBottom?: JSX.Element;
+  }
+
+  interface CardProps {
+    onSwipeLeft?: () => void;
+    onSwipeRight?: () => void;
+    onSwipeTop?: () => void;
+    onSwipeBottom?: () => void;
+  }
+}
