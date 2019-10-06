479a5c917fe171d21216f33c418e606d035b036d
勝木航 <a17dc050@dhw.ac.jp>
2019-10-07 01:21:09 +0900

    feat: fix contentBase

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   devServer: {
     open: true, //ブラウザを自動で開く
     openPage: 'index.html', //自動で指定したページを開く
-    contentBase: path.join(__dirname, 'public'), // HTML等コンテンツのルートディレクトリ
+    contentBase: dist, // HTML等コンテンツのルートディレクトリ
     watchContentBase: true, //コンテンツの変更監視をする
     port: 3000, // ポート番号
   },
