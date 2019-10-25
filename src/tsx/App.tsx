2be0dc587aa91af73bb4fa6ed689d69ecf3b2ee7
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-10-25 16:28:21 +0900

    feat: add useEffect function

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './containers/Card';
-import Img from '../img/orca.png';
+// import Img from '../img/orca.png';
 
 interface Props {}
 
 const App: React.FC<Props> = () => {
-  const cards = [Img, Img, Img];
+  // console.log(Img);
+  // const cards = [Img, Img, Img];
+  const [cards, setCards] = useState<User[]>([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      // setError(false);
+      try {
+        const QiitaUsers = await fetch('https://qiita.com/api/v2/users', {
+          method: 'GET',
+        });
+        const users: User[] = await QiitaUsers.json();
+        setCards(users);
+      } catch (error) {
+        console.log(error);
+        // setError(true);
+      }
+    };
+    getUsers();
+  }, []);
+
   return (
     <>
       <Card cards={cards}></Card>
