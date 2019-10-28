02e5a8a9fd594bf1bfa68884ef20a1c6b7599d54
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-10-28 16:38:22 +0900

    feat: add extending styles

--- src/tsx/components/Button.tsx
+++ src/tsx/components/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type Props = {
   onClickRight: () => void;
@@ -8,10 +9,36 @@ type Props = {
 const Button: React.FC<Props> = ({ onClickRight, onClickLeft }) => {
   return (
     <>
-      <button onClick={onClickLeft}>NOPE</button>
-      <button onClick={onClickRight}>LIKE</button>
+      <ButtonsStyle>
+        <ButtonStyleNope onClick={onClickLeft}>NOPE</ButtonStyleNope>
+        <ButtonStyleLike onClick={onClickRight}>LIKE</ButtonStyleLike>
+      </ButtonsStyle>
     </>
   );
 };
+
+const ButtonsStyle = styled.div`
+  position: absolute;
+  z-index: 1;
+  bottom: -30px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const ButtonStyle = styled.button`
+  width: 100px;
+  height: 100px;
+  border: none;
+  margin: 10px;
+  border-radius: 50%;
+`;
+
+const ButtonStyleNope = styled(ButtonStyle)`
+  color: #c33;
+`;
+
+const ButtonStyleLike = styled(ButtonStyle)`
+  color: #3b7;
+`;
 
 export default Button;
